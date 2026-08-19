4b4b8d635d2b95d106db9f19cf1699ecbba29b0b
badattd2019-alt <badattd2019@gmail.com>
2026-08-18 23:10:20 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 'use client';
-export const dynamic = 'force-dynamic';
-
+export default function Home() { ... }).
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2023-10-16',
 });
