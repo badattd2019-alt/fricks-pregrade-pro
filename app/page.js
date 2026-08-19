4675e2abc2cf930712ed142c5699379a5b39cb53
badattd2019-alt <badattd2019@gmail.com>
2026-08-18 23:05:11 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
-import Stripe from 'stripe';
+'use client';
+export const dynamic = 'force-dynamic';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2023-10-16',
