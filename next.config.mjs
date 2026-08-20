adbb51dbcef1fbf808cb5250f11d1cedb3cd8ec5
badattd2019-alt <badattd2019@gmail.com>
2026-08-20 02:57:41 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import withPWAInit from 'next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
